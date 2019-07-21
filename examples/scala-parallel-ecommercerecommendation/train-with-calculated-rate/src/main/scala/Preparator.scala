6ee31731139e92beae301d516768f562f7806931
s4m37 <sametoussema@gmail.com>
2019-07-21 17:51:39 +0200

    add ecom recommender template, init train-with-calculated-rate

---
+++ examples/scala-parallel-ecommercerecommendation/train-with-calculated-rate/src/main/scala/Preparator.scala
@@ -0,0 +1,28 @@
+package org.example.ecommercerecommendation
+
+import org.apache.predictionio.controller.PPreparator
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+
+class Preparator
+  extends PPreparator[TrainingData, PreparedData] {
+
+  override
+  def prepare(sc: SparkContext, trainingData: TrainingData): PreparedData = {
+    new PreparedData(
+      users = trainingData.users,
+      items = trainingData.items,
+      viewEvents = trainingData.viewEvents,
+      clickEvents = trainingData.clickEvents,
+      buyEvents = trainingData.buyEvents)
+  }
+}
+
+class PreparedData(
+  val users: RDD[(String, User)],
+  val items: RDD[(String, Item)],
+  val rateEvents: RDD[RateEvent], // MODIFIED
+  val viewEvents: RDD[ViewEvent],
+  val clickEvents: RDD[ClickEvent],
+  val buyEvents: RDD[BuyEvent]
+) extends Serializable
